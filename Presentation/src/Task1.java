1141c78db4c6cb50cbf4bc1dfcac4fb1c01ed040
Alege <elena.gerih@gmail.com>
2021-04-13 22:10:19 +0300

    first commit

---
+++ Presentation/src/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+        Task2 mainigais = new Task2("Hello");
+        System.out.println(mainigais.name);
+
+
+    }
+
+}
---
+++ Presentation/src/Task2.java
@@ -0,0 +1,8 @@
+public class Task2 {
+
+    public String name;
+
+    public Task2(String nameToChange){
+        name = nameToChange;
+    }
+}
